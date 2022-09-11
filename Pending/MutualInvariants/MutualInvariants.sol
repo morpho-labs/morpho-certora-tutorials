5dcff5dc5c2540e948a5be985477c01fd46b30be
Quentin Garchery <garchery.quentin@gmail.com>
2022-09-12 00:52:30 +0200

    feat: prover is stronk

--- Pending/MutualInvariants/MutualInvariants.sol
+++ Pending/MutualInvariants/MutualInvariants.sol
@@ -5,18 +5,21 @@ pragma solidity ^0.8.0;
 contract MutualInvariants {
     uint256[] public evens;
     uint256[] public odds;
-    uint256 public nextIndex;
+    uint256 public nextIndexEven;
+    uint256 public nextIndexOdd;
 
     constructor () {
         evens.push(0);
         odds.push(1);
+        nextIndexEven = 42;
     }
 
     function addNew() public {
-        uint even = evens[nextIndex % evens.length];
-        uint odd = odds[nextIndex % odds.length];
+        uint even = evens[nextIndexEven % evens.length];
+        nextIndexEven = uint256(keccak256(abi.encode(nextIndexEven)));
+        uint odd = odds[nextIndexOdd % odds.length];
+        nextIndexOdd = uint256(keccak256(abi.encode(nextIndexOdd)));
         evens.push(odd + 1);
         odds.push(even + 1);
-        nextIndex = uint256(keccak256(abi.encode(nextIndex)));
     }
 }
--- Pending/MutualInvariants/MutualInvariants.spec
+++ Pending/MutualInvariants/MutualInvariants.spec
@@ -1,12 +1,10 @@
 methods {
     evens(uint) returns (uint256) envfree
     odds(uint) returns (uint256) envfree
-    nextIndex() returns (uint256) envfree
+    nextIndexEven() returns (uint256) envfree
+    nextIndexOdd() returns (uint256) envfree
     addNew() envfree
 }
 
 invariant evensContainsEvenNumbers(uint256 i)
     evens(i) % 2 == 0
-
-invariant oddsContainsOddNumbers(uint256 i)
-    odds(i) % 2 == 1
